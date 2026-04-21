d92028cad7ba022bebcf12a979ceb41b65e5b4e0
dimzbro <vanzstok48@gmail.com>
2026-04-21 18:52:37 +0700

    perbaikin regis

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,19 +4,44 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['first_name', 'last_name', 'phone_number', 'email', 'password', 'is_admin', 'about_me', 'skills', 'profile_photo_path', 'rating_points', 'rating_count'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'phone_number',
+        'email',
+        'password',
+        'is_admin',
+        'about_me',
+        'skills',
+        'profile_photo_path',
+        'rating_points',
+        'rating_count',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+    
     public function getAverageRatingAttribute()
     {
         if ($this->rating_count == 0) {
